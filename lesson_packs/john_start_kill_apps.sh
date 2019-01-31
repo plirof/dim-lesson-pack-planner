03aa074cf3e88c0c440f5311ddb3634b46c36a3a
aaa <aaa@aaa.com>
2019-01-31 09:00:55 +0200

    startup scripts

---
+++ lesson_packs/john_start_kill_apps.sh
@@ -0,0 +1,98 @@
+# kill firefox (if you want )
+pkill -f firefox
+#ironstart https://studio.code.org/s/course1
+#ironstart https://studio.code.org/s/course1/stage/4/puzzle/10
+#ironstart https://studio.code.org/s/course1/stage/7/puzzle/10
+
+CODEorg=https://studio.code.org/s/course1/stage/
+LANDINGpages="$LANDINGpages"
+SWFlocal="http://192.168.1.200/swf/"
+OOOKIDS="ooo4kids1.3 -n "
+OOOKIDSpathprefix="/opt/lampp/htdocs/askiseis_office/"
+
+
+ironstart() {
+# eg  ironstart ""$SWFpath"pack_A01.html "$LANDINGpage"15-ΤΕΤΑΡΤΗ-Β1--hour5.html "$SWFpath"pack_A01.html"
+ironstart "$1"
+return 5
+}
+
+oookidsopen()
+{
+# eg oookidsopen "OFFICE_extra_files/pliktrologisi_j02.1_antigrafi_katanomi_wron.doc"
+oookidsopen "$1"
+return 5
+}
+
+
+#. /usr/local/bin/tuxpaint-with-conf.sh
+#ironstart ""$SWFlocal"swf_giortes/christmas.html http://192.168.1.200/landing_pages/PEMPTH-ST1--hour3__dim10.html"
+#. /usr/local/bin/tuxpaint-with-conf.sh
+#ironstart ""$SWFlocal"swf_giortes/christmas.html http://192.168.1.200/landing_pages/PEMPTH-E2--hour6__dim10.html"
+
+
+#ironstart ""$SWFlocal"swf_giortes/christmas.html http://192.168.1.200/landing_pages/ΤΕΤΑΡΤΗ-Γ2--hour1__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+# A1
+#ironstart ""$SWFlocal"swf_giortes/christmas.html "$LANDINGpages"ΤΕΤΑΡΤΗ-Α1--hour2__dim10.html  "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+
+
+: '#ΤΕΤΑΡΤΗ 10ο ΔΗΜ
+#        -----------Γ2--hour1-----------
+#$OOOKIDS
+oookidsopen "OFFICE_extra_files/pliktrologisi_j01.1_antigrafi_stratigiki_ekmathisis.doc"
+#ironstart ""$CODEorg"10/puzzle/1 http://192.168.1.200/landing_pages/ΤΕΤΑΡΤΗ-Γ2--hour1__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+#        -----------A1-----------
+#ironstart ""$SWFlocal"ab/funbrain.html "$LANDINGpages"ΤΕΤΑΡΤΗ-Α1--hour2__dim10.html  "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+ironstart ""$SWFlocal"pack_A01.html http://192.168.1.200/landing_pages/PEMPTH-E2--hour6__dim10.html"
+#/opt/eduActiv8/eduActiv8
+#ΤΕΤΑΡΤΗ 10ο ΔΗΜ '
+
+#: '#ΠΕΜΠΤΗ 10ο ΔΗΜ
+#ΠΕΜΠΤΗ------------Δ2--hour1-----------
+#ironstart ""$CODEorg"16/puzzle/1 "$LANDINGpages"ΠΕΜΠΤΗ-Δ2--hour1__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+oookidsopen "OFFICE_extra_files/pliktrologisi_j01.1_antigrafi_stratigiki_ekmathisis.doc"
+#ΠΕΜΠΤΗ------------Γ1--hour2-----------
+#ironstart ""$CODEorg"16/puzzle/1 "$LANDINGpages"ΠΕΜΠΤΗ-Γ1--hour2__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+oookidsopen "OFFICE_extra_files/pliktrologisi_j01.1_antigrafi_stratigiki_ekmathisis.doc"
+#ΠΕΜΠΤΗ------------ΣΤ1--hour3-----------
+#ironstart ""$CODEorg"14/puzzle/6 "$LANDINGpages"ΠΕΜΠΤΗ-ΣΤ1--hour3__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+oookidsopen "OFFICE_extra_files/pliktrologisi_j01.1_antigrafi_stratigiki_ekmathisis.doc"
+#ΠΕΜΠΤΗ------------Δ1--hour5-----------
+#ironstart ""$CODEorg"13/puzzle/8 "$LANDINGpages"ΠΕΜΠΤΗ-Δ1--hour5__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+oookidsopen "OFFICE_extra_files/pliktrologisi_j01.1_antigrafi_stratigiki_ekmathisis.doc"
+#ΠΕΜΠΤΗ------------Ε2--hour6-----------
+#ironstart ""$CODEorg"13/puzzle/10 "$LANDINGpages"ΠΕΜΠΤΗ-Ε2--hour6__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+oookidsopen "OFFICE_extra_files/pliktrologisi_j02.1_antigrafi_katanomi_wron.doc"
+ #ΠΕΜΠΤΗ 10ο ΔΗΜ ' 
+
+: '#ΠΑΡΑΣΚΕΥΗ 10ο ΔΗΜ
+# -----------A2--hour2# -----------
+ironstart ""$SWFlocal"pack_A01.html "$LANDINGpages"ΠΑΡΑΣΚΕΥΗ-A2--hour2__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+#/opt/eduActiv8/eduActiv8
+#"$LANDINGpages"ΠΑΡΑΣΚΕΥΗ-A2--hour2__dim10.html  "$SWFlocal"fun/maze/Kaban_agriogourouno_maze_ok_more_time!!.swf"
+#  -----------ΣΤ2--hour# -----------
+oookidsopen "OFFICE_extra_files/pliktrologisi_j02.1_antigrafi_katanomi_wron.doc"
+#ironstart ""$CODEorg"13/puzzle/1 "$LANDINGpages"ΠΑΡΑΣΚΕΥΗ-ΣΤ2--hour3__dim10.html tortuga.dimotiko.tk "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+# -----------Ε1--hour5-----------
+oookidsopen "OFFICE_extra_files/pliktrologisi_j02.1_antigrafi_katanomi_wron.doc"
+#ironstart ""$CODEorg"13/puzzle/1 "$LANDINGpages"ΠΑΡΑΣΚΕΥΗ-Ε1--hour5__dim10.html tortuga.dimotiko.tk "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+# -----------Β--hour6-----------
+/opt/eduActiv8/eduActiv8
+ironstart ""$SWFlocal"pack_A01.html "$LANDINGpages"ΠΑΡΑΣΚΕΥΗ-Β--hour6__dim10.html "$SWFlocal"fun/finding-santa__christmas_find_noAdURL_!!.swf"
+#ΠΑΡΑΣΚΕΥΗ 10ο ΔΗΜ'
+
+
+
+#ironstart ""$CODEorg"13/puzzle/1 http://192.168.1.200/ΔΕΥΤΕΡΑ-Γ1--hour3 http://tortuga.dimotiko.tk"
+#ironstart ""$CODEorg"10/puzzle/1 http://192.168.1.200/ΔΕΥΤΕΡΑ-Γ2--hour4 http://tortuga.dimotiko.tk"
+#ironstart ""$CODEorg"13/puzzle/1 http://192.168.1.200/ΔΕΥΤΕΡΑ-ΣΤ2--hour5 http://tortuga.dimotiko.tk"
+#ironstart ""$CODEorg"13/puzzle/1 http://192.168.1.200/ΔΕΥΤΕΡΑ-ΣΤ1--hour6 http://tortuga.dimotiko.tk"
+
+# following opens many tabs (note the double quotes "")
+#. iron_flash_puppy_pepper_sda1.sh ""$CODEorg"8/puzzle/1 https://studio.code.org/s/course1"
+
+#ZOGRAFIKI
+#. /usr/local/bin/tuxpaint-with-conf.sh
+
+#next blocky http://192.168.1.200/gamesedu/blockly-games/el/index.html
+#lightbot select http://192.168.1.200/gamesedu/lightbot_haan/select.php  
