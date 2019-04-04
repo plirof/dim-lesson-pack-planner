a573430f7ad0db28419eb7ab460beab1193bf8e1
aaa <aaa@aaa.com>
2019-04-04 16:42:57 +0300

    190404b showdiv

--- lesson_packs/pack_js_footer.js
+++ lesson_packs/pack_js_footer.js
@@ -2,6 +2,7 @@
 /*
 * pack_js_footer  (put this in same folder as parent html)
 *
+* ver190404 -showdiv
 * ver190402 -all scripts are now in footer
 * 
 * 
@@ -27,35 +28,52 @@
     var additional_wait_minutes=additional_wait_time*1000*60;
     // ))))))))))))))))  preset timers )))))))))))))))))))))))
 
+    // (((((((((((((((((((  option to show only specific DIV (((((((((((((((((((
+    var url_showdiv_param=location.search.substring(1).indexOf("showdiv");// != to -1 if we have this param
+    var url_showdiv1=location.search.substring(1).indexOf("showdiv1");
+    var url_showdiv2=location.search.substring(1).indexOf("showdiv2");
+    var url_showdiv3=location.search.substring(1).indexOf("showdiv3");
+    var url_showdiv4=location.search.substring(1).indexOf("showdiv4");
+    var url_showdiv5=location.search.substring(1).indexOf("showdiv5");
+    var url_showdiv6=location.search.substring(1).indexOf("showdiv6");
+    var url_showdiv7=location.search.substring(1).indexOf("showdiv7");
+    // ))))))))))))))))  option to show only specific DIV  )))))))))))))))))))))))
 
 
-    //if we find a parameter that contains word "time" (eg packA01.html?timer) then we hide all the other links
+
+
+//if we find a parameter that contains word "time" (eg packA01.html?timer) then we hide all the other links
 function init_links(){
-    if(url_time_param!==-1 ) {
 
-document.getElementById("div2").style.visibility = "hidden";
-document.getElementById("div3").style.visibility = "hidden";
-document.getElementById("div4").style.visibility = "hidden";
-document.getElementById("div5").style.visibility = "hidden";
-document.getElementById("div6").style.visibility = "hidden";
-document.getElementById("div7").style.visibility = "hidden";
-        
-        /*document.getElementById("div2").style.visibility = "hidden";
+    if((url_showdiv_param!==-1 ) && (url_time_param==-1 ) ){  //disable show_url IF we have set timer
+        //if((url_showdiv1==-1) && (url_time_param==-1 )) document.getElementById("div1").style.visibility = "hidden";
+        if(url_showdiv1==-1) document.getElementById("div1").style.visibility = "hidden";
+        if(url_showdiv2==-1) document.getElementById("div2").style.visibility = "hidden";
+        if(url_showdiv3==-1) document.getElementById("div3").style.visibility = "hidden";
+        if(url_showdiv4==-1) document.getElementById("div4").style.visibility = "hidden";
+        if(url_showdiv5==-1) document.getElementById("div5").style.visibility = "hidden";
+        if(url_showdiv6==-1) document.getElementById("div6").style.visibility = "hidden";
+        if(url_showdiv7==-1) document.getElementById("div7").style.visibility = "hidden";    
+    }    
+
+    //If we have a timer URL param then HIDE divs
+    if(url_time_param!==-1 ) {
+        document.getElementById("div2").style.visibility = "hidden";
         document.getElementById("div3").style.visibility = "hidden";
         document.getElementById("div4").style.visibility = "hidden";
         document.getElementById("div5").style.visibility = "hidden";
-        */
-        //document.getElementById("div6").style.visibility = "hidden";
-        
+        document.getElementById("div6").style.visibility = "hidden";
+        document.getElementById("div7").style.visibility = "hidden";
+        //document.getElementById("div6").style.visibility = "hidden";        
     }
-}
+
+
+} // end of function init_links(){
 
 function showItTimer() {
-
+    //enable if its 1st click AND we have a timer UTL param
     if(first_click && url_time_param !==-1 ) {
         //alert("first_timer=true   , "+"location.search.substring(1)="+location.search.substring(1));
-        
-    
         //if(location.search.substring(1)>0)  alert("location.search.substring(1)="+location.search.substring(1));
         var minute=1000*60;
         console.log("first click , timers set every :");
@@ -67,13 +85,12 @@ function showItTimer() {
             setTimeout(function(){document.getElementById("div4").style.visibility = "visible"}, minute/60);//test
             setTimeout(function(){document.getElementById("div4").style.visibility = "hidden"}, 3000);//test
             // ########### TEST #####################
-
-setTimeout(function(){document.getElementById("div2").style.visibility = "visible"}, 5*minute+additional_wait_minutes);
-setTimeout(function(){document.getElementById("div3").style.visibility = "visible"}, 10*minute+additional_wait_minutes);
-setTimeout(function(){document.getElementById("div4").style.visibility = "visible"}, 15*minute+additional_wait_minutes);
-setTimeout(function(){document.getElementById("div5").style.visibility = "visible"}, 20*minute+additional_wait_minutes);
-setTimeout(function(){document.getElementById("div6").style.visibility = "visible"}, 25*minute+additional_wait_minutes);
-setTimeout(function(){document.getElementById("div7").style.visibility = "visible"}, 26*minute+additional_wait_minutes);
+        setTimeout(function(){document.getElementById("div2").style.visibility = "visible"}, 5*minute+additional_wait_minutes);
+        setTimeout(function(){document.getElementById("div3").style.visibility = "visible"}, 10*minute+additional_wait_minutes);
+        setTimeout(function(){document.getElementById("div4").style.visibility = "visible"}, 15*minute+additional_wait_minutes);
+        setTimeout(function(){document.getElementById("div5").style.visibility = "visible"}, 20*minute+additional_wait_minutes);
+        setTimeout(function(){document.getElementById("div6").style.visibility = "visible"}, 25*minute+additional_wait_minutes);
+        setTimeout(function(){document.getElementById("div7").style.visibility = "visible"}, 26*minute+additional_wait_minutes);
 
         
     }
@@ -81,6 +98,10 @@ setTimeout(function(){document.getElementById("div7").style.visibility = "visibl
 
     return false;
 } // end of showItTimer()
+
+
+
+
 
 
 //+++++++++++++++++++ probeserver v02 190402+++++++++++++++++
